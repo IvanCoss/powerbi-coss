6d5853d1d7aba75fe2b45082e6f6012b680b73f0
Ivan Coss <ivancoss@MacBook-Air-de-Ivan.local>
2025-07-21 19:31:14 -0600

    Agrego index.js para que Vercel construya bien

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PowerBISite from "./PowerBISite";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <PowerBISite />
+  </React.StrictMode>
+);
